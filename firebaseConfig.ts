4d33a9d813a25c8ea939101fbb5932d8a3fa97ba
Aryan Bhosale <36108149+aryanbhosale@users.noreply.github.com>
2025-01-26 17:41:16 +0530

    env

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3dB36ulPWZayKrgEBafBgI_HBINGjYWU",
-  authDomain: "bazinga-fb46b.firebaseapp.com",
-  projectId: "bazinga-fb46b",
-  storageBucket: "bazinga-fb46b.firebasestorage.app",
-  messagingSenderId: "1031632979859",
-  appId: "1:1031632979859:web:56a34fa419ad0e47930a66",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
